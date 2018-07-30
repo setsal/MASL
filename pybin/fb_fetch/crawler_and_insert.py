a6627f7a79660f579e342c12f16e0d4131132e9e
MeteorV <ekids12345@gmail.com>
2018-07-30 23:38:29 +0800

    add disp_content function

--- pybin/fb_fetch/crawler_and_insert.py
+++ pybin/fb_fetch/crawler_and_insert.py
@@ -1,12 +1,17 @@
 import sqlite3
 import datetime
 import time
-conn = sqlite3.connect('db.sqlite3') #連結指定的資料庫
+conn = sqlite3.connect('../../db.sqlite3')  # 連結指定的資料庫
 
 def disp_data():
 	cursor = conn.execute('select * from club_list;')
 	for row in cursor:
-		print("Name : {}, id : {}\n".format(row[1],row[0],row[2]))
+		print("Name : {}, id : {}\n".format(row[1],row[0]))
+
+def disp_content():
+	cursor = conn.execute('select * from fb_fetch_article;')
+	for row in cursor:
+		print("Content : {}\n".format(row[3]))
 
 def insert_data(table_name,input_arr):
 	for post in input_arr:
@@ -14,7 +19,6 @@ def insert_data(table_name,input_arr):
 		if 'message' in post.keys():
 			conn.execute("insert into fb_fetch (  textid, content ) values('{}','{}');".format( post['id'], post['message']) )
 			conn.commit()
-
 
 def setsal_insert_data( fanpage_id, table_name, input_arr ):
 	i = 2;
@@ -24,3 +28,4 @@ def setsal_insert_data( fanpage_id, table_name, input_arr ):
 			conn.execute("insert into fb_fetch_article (  uid, textid, content, created_at ) values( '{}', '{}', '{}', '{}');".format( fanpage_id, post['id'], post['message'], datetime.datetime.now()) )
 			conn.commit()
 		i = i+1
+
