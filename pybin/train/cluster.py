6b2d1d21c70809daa934071bfa8d5d97e5412d1a
Daniel <daniel@DanieldeMacBook-Pro.local>
2018-08-20 14:50:40 +0800

    modify cluster.py, now it can print the according articles for each topic

--- pybin/train/cluster.py
+++ pybin/train/cluster.py
@@ -38,12 +38,36 @@ def main():
     tfidf = models.TfidfModel(corpus)
     corpus_tfidf = tfidf[corpus]
 
+    num_topic = 3
+
     # Transfer to LSI model
-    lsi = models.LsiModel(corpus_tfidf, id2word=dictionary, num_topics=5)
+    lsi = models.LsiModel(corpus_tfidf, id2word=dictionary, num_topics=num_topic)
     corpus_lsi = lsi[corpus_tfidf]
 
+    # Get nearest topic for each article
+    topic_list = []
     for doc in corpus_lsi:
-        print(my_absmax(doc))
+        #print(my_absmax(doc))
+        topic_list.append(my_absmax(doc))
+    
+    # Sort by topic
+    topic_list_sort_by_topic = []
+    for i in range(num_topic):
+        topic_list_sort_by_topic.append([x for x, y in enumerate(topic_list) if y[0] == i])
+    #print(topic_list_sort_by_topic)
+
+    # Connect to db and print the article by id
+    conn = sqlite3.connect('../../db.sqlite3')
+    articles = []
+    for row in conn.execute('SELECT id, content FROM fb_fetch_article'):
+        articles.append(row[1])
+    
+    for i in range(num_topic)):
+        print("*********************************************************************")
+        print("Topic" + str(i + 1) + ":")
+        for id in topic_list_sort_by_topic[i]:
+            print(articles[id])
+    
 
 if __name__ == "__main__":
 	main()
