af477f17a02295a5a733889a2de8548e1ce58e55
MeteorV <ekids12345@gmail.com>
2018-09-01 16:22:25 +0800

    add favicon and add code of that

--- frontend/components/Nav.jsx
+++ frontend/components/Nav.jsx
@@ -7,6 +7,7 @@ export default class Nav extends React.Component {
     render() {
         return (
             <header className="header-area">
+            
                 <div className="container">
                     <div className="row">
                         <div className="col-12">
