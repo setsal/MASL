103081adfbbc38be96eee2839db3466aa25bbb36
Daniel <ljk25679@gmail.com>
2018-11-09 11:59:55 +0800

    testing semi-supervised lda for fb article

---
+++ pybin/train/cluster_momo.py
@@ -0,0 +1,212 @@
+# -*- coding: utf-8 -*-
+# Author: setsal Lan
+import logging
+import sqlite3
+import jieba
+import numpy as np
+import gensim
+from gensim import corpora, models, similarities
+import sys
+import io
+import os
+import pickle
+
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf8')
+
+def my_absmax(sequence):
+    if not sequence:
+        raise ValueError('empty sequence')
+
+    maximum = sequence[0]
+
+    for item in sequence:
+        # Compare elements by their weight stored in their second element.
+        if abs(item[1]) > abs(maximum[1]):
+            maximum = item
+
+    return maximum
+
+
+
+def getFbCluster():
+    logging.basicConfig(format='[%(levelname)s] : %(message)s', level=logging.INFO)
+
+    if ( os.path.exists("pybin/train/output/fb.dict") ):
+        dictionary = corpora.Dictionary.load("pybin/train/output/fb.dict")
+        corpus = corpora.MmCorpus("pybin/train/output/fb.mm")
+        logging.info("Load model success")
+    else:
+        logging.info("Please run the train2.py to create models")
+
+    # Load tf-idf model
+    tfidf = models.TfidfModel.load("pybin/train/output/fb.tfidf")
+    corpus_tfidf = tfidf[corpus]
+
+
+    num_topic = 9
+
+    # Load to LDA model
+    lda = models.LdaModel.load("pybin/train/output/fb.lda")
+    corpus_lda = lda[corpus]
+
+    # Load id list after filter
+    with open("pybin/train/output/train_id", "rb") as fp:
+        train_id = pickle.load(fp)
+
+    # Get nearest topic for each article
+    topic_list = []
+    index = 0
+    for doc in corpus_lda:
+        topic_list.append(my_absmax(doc))
+
+    # ======= topic of articles ========
+    # Connect to db and print the article by id
+    conn = sqlite3.connect('db.sqlite3')
+    articles = []
+    titles = []
+    clubs_id = []
+    for row in conn.execute('SELECT fb_fetch_article.id, fb_fetch_article.content, fb_fetch_club.name, fb_fetch_club.id FROM fb_fetch_article INNER JOIN fb_fetch_club ON fb_fetch_club.cid = fb_fetch_article.cid'):
+        articles.append(row[1])
+        titles.append(row[2])
+        clubs_id.append(row[3])
+
+    # Sort by topic
+    topic_list_sort_by_topic = []
+    for i in range(num_topic):
+         topic_list_sort_by_topic.append([x for x, y in enumerate(topic_list) if y[0] == i])
+
+
+    data = []
+    topic = {}
+    keyword_of_topic = []
+
+    for i in range(num_topic):
+        n_topic = "Topic" + str(i)
+
+        contents = []
+        idx = 0
+        for id in topic_list_sort_by_topic[i]:
+            if idx > 20:
+                break
+            single_post = {
+                'title': titles[train_id[id]],
+                'content': articles[train_id[id]],
+                'clubs_id': clubs_id[train_id[id]]
+            }
+            contents.append(single_post)
+            idx = idx + 1
+
+
+        key_list = lda.show_topic(i, topn=10)
+        temp = []
+        for tup in key_list:
+            temp2 = {
+                'value': tup[0],
+                'count': round(tup[1]*100)*6
+            }
+            temp.append(temp2)
+        keyword_of_topic.append(temp)
+
+        topic = {
+            'kind': n_topic,
+            'keyword_of_topic': keyword_of_topic[i],
+            'articles': contents
+        }
+        data.append(topic)
+
+    return(data)
+
+
+def getNewsCluster():
+    logging.basicConfig(format='[%(levelname)s] : %(message)s', level=logging.INFO)
+
+    if ( os.path.exists("pybin/train/output/news.dict") ):
+        dictionary = corpora.Dictionary.load("pybin/train/output/news.dict")
+        corpus = corpora.MmCorpus("pybin/train/output/news.mm")
+        logging.info("Load model success")
+    else:
+        logging.info("Please run the train2.py to create models")
+
+    # Load tf-idf model
+    tfidf = models.TfidfModel.load("pybin/train/output/news.tfidf")
+    corpus_tfidf = tfidf[corpus]
+
+    num_topic = 6
+
+    # Load to LDA model
+    lda = models.LdaModel.load("pybin/train/output/news.lda")
+    corpus_lda = lda[corpus]
+
+    # Get nearest topic for each article
+    topic_list = []
+    index = 0
+    for doc in corpus_lda:
+        topic_list.append(my_absmax(doc))
+
+    # ======= topic of articles ========
+    # Connect to db and print the article by id
+    conn = sqlite3.connect('db.sqlite3')
+    articles = []
+    titles = []
+    companys = []
+    categories = []
+    for row in conn.execute('SELECT media_fetch_news.id, media_fetch_news.category, media_fetch_news.title, media_fetch_news.content, media_fetch_company.name FROM media_fetch_news INNER JOIN media_fetch_company ON media_fetch_news.mid_id = media_fetch_company.id;'):
+        categories.append(row[1])
+        titles.append(row[2])
+        articles.append(row[3])
+        companys.append(row[4])
+
+    # Sort by topic
+    topic_list_sort_by_topic = []
+    for i in range(num_topic):
+        topic_list_sort_by_topic.append([x for x, y in enumerate(topic_list) if y[0] == i])
+
+
+    data = []
+    topic = {}
+    keyword_of_topic = []
+
+    for i in range(num_topic):
+        n_topic = "Topic" + str(i)
+
+
+        contents = []
+        idx = 0
+        for id in topic_list_sort_by_topic[i]:
+            if idx > 50:
+                break
+            single_post = {
+                'category': categories[id],
+                'title': titles[id],
+                'content': articles[id],
+                'company': companys[id]
+            }
+            contents.append(single_post)
+            idx = idx + 1
+
+        key_list = lda.show_topic(i, topn=10)
+        temp = []
+        for tup in key_list:
+            temp2 = {
+                'value': tup[0],
+                'count': round(tup[1]*1000)*3
+            }
+            temp.append(temp2)
+
+        keyword_of_topic.append(temp)
+
+        topic = {
+            'kind': n_topic,
+            'keyword_of_topic': keyword_of_topic[i],
+            'articles': contents
+        }
+        data.append(topic)
+
+    return(data)
+
+
+
+
+#if __name__ == "__main__":
+    #cluster = getCluster()
+    #print(cluster)
--- pybin/train/train_fb.py
+++ pybin/train/train_fb.py
@@ -10,6 +10,7 @@ from gensim import corpora, models
 import sys
 import io
 import operator
+import pickle
 from jiebaFunc import init_stopword, getArticle, getSegment
 
 
@@ -24,11 +25,6 @@ def main():
     seg_list = getSegment(article_lists)
 
     #print(seg_list)
-
-    #filt_list = [[y for y in x if y in {'FGO','fgo','少女前線','白貓','寫真','cosplay','cos','東方','演唱會','百合','艦娘','血小板','碧藍','偶像','音樂','工作細胞'}] for x in seg_list]
-
-    #print(filt_list)
-
     #'FGO','fgo','少女前線','白貓','寫真','cosplay','東方','演唱會','百合','艦娘','血小板','碧藍','偶像','音樂'
 
     train_list = []
@@ -36,7 +32,7 @@ def main():
     idx = 0
     for doc in seg_list:
         for word in doc:
-            if word in ['FGO','fgo','少女前線','白貓','寫真','cosplay','cos','東方','演唱會','百合','艦娘','血小板','碧藍','偶像','音樂','工作細胞']:
+            if word in ['FGO','fgo','Fate','少女前線','白貓','寫真','cosplay','cos','東方','演唱會','百合','艦娘','血小板','碧藍','音樂','工作細胞']:
                 train_list.append(doc)
                 train_id.append(idx)
                 break
@@ -45,25 +41,31 @@ def main():
     #print(train_list)
     #print(train_id)
 
+    filt_list = [[y for y in x if y in {'FGO','fgo','Fate','少女前線','白貓','寫真','cosplay','cos','東方','演唱會','百合','艦娘','血小板','碧藍','音樂','工作細胞'}] for x in train_list]
+    #print(filt_list)
     
-    n = 30
+    with open("output/train_id", "wb") as fp:
+       pickle.dump(train_id, fp)
+
+    n = 20
 
     # 移除只出現n次的字詞
     from collections import defaultdict
     frequency = defaultdict(int)
-    for text in train_list:
+    for text in filt_list:
         for token in text:
             frequency[token] += 1
     
     
+    """
     sorted_frequency = sorted(frequency.items(), key=operator.itemgetter(1), reverse=True)
     for word in sorted_frequency:
         if word[1] >= 30:
             print(word[0]+":"+str(word[1]))
+    """
     
-
     texts = [[token for token in text if frequency[token] >= n]
-             for text in train_list]
+             for text in filt_list]
 
     # Create dictionary
     dictionary = corpora.Dictionary(texts)
@@ -75,27 +77,28 @@ def main():
     corpus = [dictionary.doc2bow(text) for text in texts]
     corpora.MmCorpus.serialize("output/fb.mm", corpus)
     logging.info("Create data flow success.")
-
     
+    """
     tfidf = models.TfidfModel(corpus)
     tfidf.save("output/fb.tfidf")
     corpus_tfidf = tfidf[corpus]
     logging.info("Create TF-IDF model success.")
+    """
     
-
-    num_topic = 6
+    num_topic = 9
 
     # Transfer to LSI model
     lda = models.LdaModel(corpus, id2word=dictionary, num_topics=num_topic, iterations=100, passes=20)
     lda.save("output/fb.lda")
     logging.info("Create LDA model success.")
 
-    """
+    
     for i in range(lda.num_topics):
         print(lda.print_topic(i))
         print('\n')
-    """
     
+    
+    """
     keyword_of_topic = []
     for i in range(lda.num_topics):
         key_list = lda.show_topic(i, topn=10)
@@ -105,6 +108,7 @@ def main():
         keyword_of_topic.append(temp)
     
     print(keyword_of_topic)
+    """
 
 if __name__ == "__main__":
 	main()
